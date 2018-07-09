960dfb4aaca4892c6dcf17438967fa0ab8707de4
Maliek <maliek.meersschaert@hotmail.com>
2018-07-09 14:53:48 +0200

    change script

--- install-cylance.sh
+++ install-cylance.sh
@@ -3,9 +3,11 @@
 
 TOKEN=$1
 
+
+
 mkdir -p /opt/cylance
 cd /opt/cylance
 touch config_defaults.txt
 echo InstallToken=$TOKEN > config_defaults.txt
 
-yum install CylancePROTECT.el7.rpm CylancePROTECTUI.el7.rpm
+yum install $2 $3
