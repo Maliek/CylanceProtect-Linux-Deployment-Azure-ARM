40b6e7f745002c0653b8a57a56c80d648bd67e0a
Maliek <maliek.meersschaert@hotmail.com>
2018-07-10 10:55:31 +0200

    add install

--- install-cylance.sh
+++ install-cylance.sh
@@ -2,10 +2,25 @@
 # Pass TOKEN as parameter
 
 TOKEN=$1
+ARTIFACTSLOCATION=$2
+ARTIFACTSLOCATIONSASTOKEN=$3
+FILEONE="\.\CylancePROTECT.el7.rpm"
+FILETWO="\.\CylancePROTECTUI.el7.rpm"
+
+SOURCEONE=$ARTIFACTSLOCATION
+SOURCEONE+=$FILEONE
+SOURCEONE+=$ARTIFACTSLOCATIONSASTOKEN
+
+SOURCETWO=$ARTIFACTSLOCATION
+SOURCETWO+=$FILETWO
+SOURCETWO+=$ARTIFACTSLOCATIONSASTOKEN
 
 mkdir -p /opt/cylance/
 cd /opt/cylance/
 touch config_defaults.txt
 echo InstallToken=$TOKEN > config_defaults.txt
 
-# yum install $2 $3
+wget -O /opt/cylance/CylancePROTECT.el7.rpm "$SOURCEONE"
+wget -O /opt/cylance/CylancePROTECTUI.el7.rpm "$SOURCETWO"
+
+yum install CylancePROTECT.el7.rpm CylancePROTECTUI.el7.rpm
