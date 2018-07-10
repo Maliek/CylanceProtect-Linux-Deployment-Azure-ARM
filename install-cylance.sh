c22d0f6292b0a64a60c5d0fc09fb460f4747cd32
Maliek <maliek.meersschaert@hotmail.com>
2018-07-10 11:07:18 +0200

    direct download from github

--- install-cylance.sh
+++ install-cylance.sh
@@ -2,25 +2,13 @@
 # Pass TOKEN as parameter
 
 TOKEN=$1
-ARTIFACTSLOCATION=$2
-ARTIFACTSLOCATIONSASTOKEN=$3
-FILEONE="\.\CylancePROTECT.el7.rpm"
-FILETWO="\.\CylancePROTECTUI.el7.rpm"
-
-SOURCEONE=$ARTIFACTSLOCATION
-SOURCEONE+=$FILEONE
-SOURCEONE+=$ARTIFACTSLOCATIONSASTOKEN
-
-SOURCETWO=$ARTIFACTSLOCATION
-SOURCETWO+=$FILETWO
-SOURCETWO+=$ARTIFACTSLOCATIONSASTOKEN
 
 mkdir -p /opt/cylance/
 cd /opt/cylance/
 touch config_defaults.txt
 echo InstallToken=$TOKEN > config_defaults.txt
 
-wget -O /opt/cylance/CylancePROTECT.el7.rpm "$SOURCEONE"
-wget -O /opt/cylance/CylancePROTECTUI.el7.rpm "$SOURCETWO"
+wget -O /opt/cylance/CylancePROTECT.el7.rpm "https://github.com/Maliek/CylanceProtect-Linux-Deployment-Azure-ARM/raw/master/CylancePROTECT.el7.rpm"
+wget -O /opt/cylance/CylancePROTECTUI.el7.rpm "https://github.com/Maliek/CylanceProtect-Linux-Deployment-Azure-ARM/raw/master/CylancePROTECTUI.el7.rpm"
 
 yum install CylancePROTECT.el7.rpm CylancePROTECTUI.el7.rpm
