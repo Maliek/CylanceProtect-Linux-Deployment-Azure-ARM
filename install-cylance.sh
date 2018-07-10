afbb7ae735aa2a563b05a1365880be48b7880753
Maliek <maliek.meersschaert@hotmail.com>
2018-07-10 11:21:41 +0200

    yum older 2 week repo fix

--- install-cylance.sh
+++ install-cylance.sh
@@ -11,4 +11,6 @@ echo InstallToken=$TOKEN > config_defaults.txt
 wget -O /opt/cylance/CylancePROTECT.el7.rpm "https://github.com/Maliek/CylanceProtect-Linux-Deployment-Azure-ARM/raw/master/CylancePROTECT.el7.rpm"
 wget -O /opt/cylance/CylancePROTECTUI.el7.rpm "https://github.com/Maliek/CylanceProtect-Linux-Deployment-Azure-ARM/raw/master/CylancePROTECTUI.el7.rpm"
 
+yum makecache fast
+
 yum install CylancePROTECT.el7.rpm CylancePROTECTUI.el7.rpm
